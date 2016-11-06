81e8ad2df3fd8d54292963a31f47ac08f2e140c0
Chris Lanus <cqlanus@gmail.com>
2016-11-05 17:28:03 -0700

    scroll spy and smooth scrolling

--- js/script.js
+++ js/script.js
@@ -2,3 +2,27 @@ $(document).on('click', '[data-toggle="lightbox"]', function(event) {
     event.preventDefault();
     $(this).ekkoLightbox();
 });
+
+
+// Add an inbody class to nav
+$('.navbar-fixed-top').on('activate.bs.scrollspy', function(){
+  var hash = $(this).find('li.active a').attr('href');
+  if(hash !== '#homePage'){
+    $('nav').addClass('inBody');
+  } else {
+    $('nav').removeClass('inBody');
+  }
+})
+
+$('.navbar [href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
